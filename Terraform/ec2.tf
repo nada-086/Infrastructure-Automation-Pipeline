8585ed143e9f65b717d9bb2b2ba9948366e8b67b
Nada Essa <nadaessa086@gmail.com>
2025-04-14 11:03:35 +0200

    Changing the method of declaring the security groups in the ec2 instances

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "jenkins_ec2" {
     ami                    = var.ami
     instance_type          = var.instance_type
     key_name               = var.key_file
-    security_groups        = [var.ssh_security_group]
+    vpc_security_group_ids = [aws_security_group.ec2_ssh_sg.id]
     associate_public_ip_address = var.associate_public_ip_address
 
     tags = {
